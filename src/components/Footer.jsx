18c4ad94a524e386da1d39f53fd9f54ef45b702d
Sumani <123summanijha@gmail.com>
2023-11-08 00:34:10 +0530

    made a design of upcoming events and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,46 @@
-import React from "react";
-import "../styles/Footer.css";
+import React from 'react';
+import '../styles/Footer.css';
 
-export const Footer = () => {
-  return (
-    <>
-      <div id="footer">footer</div>
-    </>
-  );
-};
-export default Footer;
+function Footer() {
+    return (
+      <footer>
+        <div className="footer-section">
+          <h4>Navigate</h4>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/associates">Associates</a></li>
+          </ul>
+        </div>
+        <div className="footer-section">
+          <h4>About Us</h4>
+          <ul>
+            <li><a href="/porfolio">Portfolio</a></li>
+            <li><a href="/alumni">Alumni</a></li>
+          </ul>
+        </div>
+        <div className="footer-section">
+          <h4>Initiatives</h4>
+          <ul>
+            <li><a href="/xyz">XYZ</a></li>
+            <li><a href="/abc">ABC</a></li>
+          </ul>
+        </div>
+        <div className='footer-sec2'>
+            <p>Copyright &copy; 2023, E-cell, NIT Jamshedpur</p>
+        </div>
+        <div className='footer-sec2'>
+            <a href='ecell@nitjsr.ac.in'>ecell@nitjsr.ac.in</a>
+        </div>
+        <div className='footer-sec2'>
+            <a href='www.facebook.com'>Facebook</a>
+            <a href='www.instagram.com'>Instagram</a>
+            <a href='www.twitter.com'>Twitter</a>
+            <a href='www.linkdin.com'>LinkdIn</a>
+        </div>
+      </footer>
+    );
+  }
+  
+
+
+export default Footer
--- src/components/UpcomingEvent.jsx
+++ src/components/UpcomingEvent.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import Card from './Card';
 import "../styles/UpcomingEvent.css";
 
 export const UpcomingEvent = () => {
   return (
-    <>
-      <div id="upcomingevent">UpcomingEvents</div>
-    </>
+    
+    <div className="App">
+      <h2>Upcoming events</h2>
+      <Card color="red" imageSrc="image1.jpg" text="Business Tycoon" />
+      <Card color="blue" imageSrc="image2.jpg" text="Corporate Roadies" />
+      <Card color="green" imageSrc="image3.jpg" text="
+      idea Hackathon" />
+      <Card color="yellow" imageSrc="image4.jpg" text="Star Speaker" />
+      <Card color="orange" imageSrc="image5.jpg" text="Teenprenur" />
+    </div>
   );
 };
 export default UpcomingEvent;

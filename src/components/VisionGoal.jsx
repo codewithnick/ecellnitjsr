9e88fe0f239447a7d338a614dfceafee2c5afb17
Altair <deygourav3@gmail.com>
2023-11-07 22:58:08 +0530

    Added the vision and Goals Section (--Altair)

--- src/components/VisionGoal.jsx
+++ src/components/VisionGoal.jsx
@@ -4,7 +4,44 @@ import "../styles/VisionGoal.css";
 export const VisionGoal = () => {
   return (
     <>
-      <div id="visiongoal">Vision & Goals</div>
+      <div id="visiongoal" className="center-align">
+        Vision & Goals
+        </div>
+      
+      <div className="container">
+        <div className="card">
+          <div className="box">
+            <div className="content">
+              <h2>01</h2>
+              <h3>Card One</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
+              <a href="#">Read More</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="box">
+            <div className="content">
+              <h2>02</h2>
+              <h3>Card Two</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
+              <a href="#">Read More</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="box">
+            <div className="content">
+              <h2>03</h2>
+              <h3>Card Three</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
+              <a href="#">Read More</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };

a63c2a4f82abff7bff4e38700bc451acb87836a8
Altair <deygourav3@gmail.com>
2023-11-08 22:16:02 +0530

    Removed the no.

--- src/components/VisionGoal.jsx
+++ src/components/VisionGoal.jsx
@@ -12,7 +12,7 @@ export const VisionGoal = () => {
         <div className="card">
           <div className="box">
             <div className="content">
-              <h2>01</h2>
+             
               <h3>Mission</h3>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
               <a href="#">Read More</a>
@@ -23,7 +23,7 @@ export const VisionGoal = () => {
         <div className="card">
           <div className="box">
             <div className="content">
-              <h2>02</h2>
+             
               <h3>Vision</h3>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam velit? Iure nemo labore inventore?</p>
               <a href="#">Read More</a>
@@ -34,7 +34,7 @@ export const VisionGoal = () => {
         <div className="card">
           <div className="box">
             <div className="content">
-              <h2>03</h2>
+             
               <h3>Goal</h3>
               <p>totam velit? Iure nemo labore inventore?</p>
               <a href="#">Read More</a>

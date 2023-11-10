e67976e25b028f0a998369aa700a275645d33c44
Gourab Bistu <gourabbistu2002@gmail.com>
2023-11-10 21:38:42 +0530

    Adding pastEventcomponent

--- src/components/PastEvent.jsx
+++ src/components/PastEvent.jsx
@@ -48,7 +48,6 @@ const PastEvent = () => {
               className="pastEvents"
               src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
               alt="image 1"
-              style={{ backgroundColor: 'white' }} 
             />
             <div className="carousel-caption">
               <h5>This is event 1 </h5>

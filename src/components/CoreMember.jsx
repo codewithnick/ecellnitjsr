fff4f26592705418708b2f25c600f908ffeec76d
harshit960 <raj.harshit960@gmail.com>
2023-11-12 13:17:31 +0530

    centered

--- src/components/CoreMember.jsx
+++ src/components/CoreMember.jsx
@@ -8,45 +8,9 @@ export const CoreMember = () => {
         <div className="w-full text-center text-3xl font-bold p-5 text-red-600">
           Core Members
         </div>
-        <div className="w-full px-52">
-        <div className="flex flex-wrap w-full text-center justify-between">
+        <div className="flex justify-center w-full text-center">
+        <div className="flex flex-wrap max-w-max text-center justify-between">
         
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow w-80 m-5">
-          <a href="/">
-            <img class="rounded-t-lg" src="https://img.freepik.com/premium-vector/people-profile-graphic_24911-21373.jpg" alt="" />
-          </a>
-          <div class="p-2 text-center">
-            <a href="/">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">XYZ</h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">GEN SEC</p>
-            
-          </div>
-        </div>
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow w-80 m-5">
-          <a href="/">
-            <img class="rounded-t-lg" src="https://img.freepik.com/premium-vector/people-profile-graphic_24911-21373.jpg" alt="" />
-          </a>
-          <div class="p-2 text-center">
-            <a href="/">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">XYZ</h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">GEN SEC</p>
-            
-          </div>
-        </div>
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow w-80 m-5">
-          <a href="/">
-            <img class="rounded-t-lg" src="https://img.freepik.com/premium-vector/people-profile-graphic_24911-21373.jpg" alt="" />
-          </a>
-          <div class="p-2 text-center">
-            <a href="/">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">XYZ</h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">GEN SEC</p>
-            
-          </div>
-        </div>
         <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow w-80 m-5">
           <a href="/">
             <img class="rounded-t-lg" src="https://img.freepik.com/premium-vector/people-profile-graphic_24911-21373.jpg" alt="" />

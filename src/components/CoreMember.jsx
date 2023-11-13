374bc637c4c489d3c17d5b31a95760bc95e8c2a4
harshit960 <raj.harshit960@gmail.com>
2023-11-13 18:31:21 +0530

    coreMember added

--- src/components/CoreMember.jsx
+++ src/components/CoreMember.jsx
@@ -2,21 +2,22 @@ import React from "react";
 import "../styles/CoreMember.css";
 
 const Members = [
-  {id:1, name:'Subrajeet',por:'',pic:"members\\Subrajeet.JPG"},
-  {id:1, name:'Anshu Kumar',por:'',pic:"members\\anshu kumar CRN head.jpg"},
-  {id:1, name:'Nikhil Singh',por:'',pic:"members\\Nikhil singh.jpg"},
-  {id:1, name:'Nitesh Kumar',por:'',pic:"members\\Nitish kumar Web head.jpg"},
-  {id:1, name:'Rambabu Kumar',por:'',pic:"members\\Rambabu kumar.jpg"},
-  {id:1, name:'Suraj Burnwal',por:'',pic:"members\\suraj burnwal crn head.jpg"},
-  {id:1, name:'Tushar Kumar',por:'',pic:"members\\tushar kumar pr head.jpg"},
-  {id:1, name:'Yashraj SIngh',por:'',pic:"members\\Yashraj singh .jpg"},
+  {id:1, name:'Anish',por:'President',pic:"members\\Rambabu kumar.jpg"},
+  {id:1, name:'Subrajeet',por:'Vice President',pic:"members\\Subrajeet.JPG"},
+  {id:1, name:'Yashraj Singh',por:'General Secretary',pic:"members\\Yashraj singh .jpg"},
+  {id:1, name:'Anshu Kumar',por:'CRN Head',pic:"members\\anshu kumar CRN head.jpg"},
+  {id:1, name:'Suraj Burnwal',por:'CRN Head',pic:"members\\suraj burnwal crn head.jpg"},
+  {id:1, name:'Nikhil Singh',por:'Operations Head',pic:"members\\Nikhil singh.jpg"},
+  {id:1, name:'Rambabu',por:'Operations Head',pic:"members\\Rambabu.jpg"},
+  {id:1, name:'Tushar Kumar',por:'PR Head External',pic:"members\\tushar kumar pr head.jpg"},
+  {id:1, name:'Nitesh Kumar',por:'Web Team Head',pic:"members\\Nitish kumar Web head.jpg"},
 ]
 
 export const CoreMember = () => {
   return (
     <>
       <div className="coremember bg-white">
-        <div className="w-full text-center text-3xl font-bold p-5 text-red-600">
+        <div className="w-full text-center text-3xl font-bold p-10 text-red-600">
           Core Members
         </div>
         <div className="flex justify-center w-full text-center">
@@ -30,7 +31,7 @@ export const CoreMember = () => {
             <a href="/">
               <h5 class="mb-2 text-xl font-medium tracking-tight text-gray-900">{member.name}</h5>
             </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">GEN SEC</p>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{member.por}</p>
             
           </div>
         </div>

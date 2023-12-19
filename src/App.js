a0cda45dae4dc1e3b702d7fd3706b62c8edcead0
Abhay Kumar Sharma <77601553+abhay5624@users.noreply.github.com>
2023-12-19 23:40:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import Register from "./components/Register";
 import Footer from "./components/Footer";
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import About from './components/About';
-import Gallery from './components/Gallery';
 
 const router = createBrowserRouter([
   {
@@ -22,7 +21,6 @@ const router = createBrowserRouter([
       <HeroSection />
       <VisionGoal />
       <ProfIncharge />
-      <Gallery/>
       <Footer />
     </>
   },
